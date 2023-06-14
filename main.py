64643d4bdd710a5258821c9fe88868b49cf98828
KhachKara <khachatur2000@mail.ru>
2023-06-14 13:49:06 +0300

    working features
    start
    donate
    sending an answer message

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ from telebot.types import Message
 from envparse import Env
 from telebot.types import Message
 import json
+import requests
+from datetime import datetime
 
 env = Env()
 TOKEN = env.str('TOKEN')
+ADMIN_CHAT_ID = env.int('ADMIN_CHAT_ID')
 bot_link = env.str('bot_link')
 bot_search_name = env.str('bot_search_name')
+
 
 bot_client = telebot.TeleBot(token=TOKEN)
 
 
-@bot_client.message_handler(commands=['start', 'donate'])
-def echo(message: Message):
+@bot_client.message_handler(commands=['start'])
+def start(message: Message):
     with open('users.json', 'r') as f_o:
         data_from_json = json.load(f_o)
 
@@ -27,7 +31,24 @@ def echo(message: Message):
         data_from_json[user_id] = {'username': username}
     with open('users.json', 'w') as f_o:
         json.dump(data_from_json, f_o, indent=4, ensure_ascii=False)
-    bot_client.reply_to(message=message, text=str(f'Приветсвую {user_name}! Ваш userid: {user_id}'))
+    bot_client.reply_to(message=message, text=f'Приветсвую {username}! Ваш userid: {user_id}')
 
 
-bot_client.polling()
+
+def answer_handle(message: Message):
+    bot_client.reply_to(message, text='Спасибо за инвестиции')
+
+
+@bot_client.message_handler(commands=['donate'])
+def donate(message: Message):
+    bot_client.reply_to(message, text='Какой бюджет вы рассматриваете для своего проекта инвестиций?')
+    bot_client.register_next_step_handler(message, answer_handle)
+
+
+while True:
+    try:
+        bot_client.polling()
+    except JSONDecodeError as err:
+        print('Ошибка', err)
+        requests.post(f'https://core.telegram.org/bot{TOKEN}'
+                      f'/sendMessage?chat_id=42791670&text={datetime.now()} ::: {err.__class__} ::: {err}')
